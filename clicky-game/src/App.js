8d2823d6e737b18890b5e7b0907312769da2097b
Nicole Essex <Essex@nicoleesexsmbp2.uconnect.utah.edu>
2018-06-13 18:33:08 -0600

    needs work

---
+++ clicky-game/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+// import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+// import Randomize from "./components/Randomize";
+// import Images from "./images.json";
+import Counter from "./components/Counter";
+import Subtitle from "./components/Subtitle";
+import ImageDisplay from "./components/ImageDisplay/ImageDisplay.js";
+
+const App = () => 
+  
+  <div>
+    <Title/>
+    <Subtitle/>
+    <Counter/>
+    <ImageDisplay/>
+
+  </div>
+  
+  
+  
+  
+   
+
+
+
+
+  
+
+
+export default App;
---
+++ clicky-game/src/components/Subtitle.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Subtitle = () => {
+
+    return <h2 className="subtitle"> Try to choose a different picture each time until you've chosen each of them once.</h2> 
+  }
+
+  export default Subtitle;
